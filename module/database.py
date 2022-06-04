ba37b9d601a823114f43bda21c1516de3fd37fc7
Ghazi Taqiyya Al Anshari <64888591+taqiyyaghazi@users.noreply.github.com>
2022-06-04 18:48:50 +0700

    Update database.py

--- module/database.py
+++ module/database.py
@@ -2,9 +2,9 @@ import pymongo
 from pymongo import MongoClient
 
 def handling_msg_missed(message):
-    client = pymongo.MongoClient("mongodb+srv://taqiyyaghazi:7bgnKzVxsG77U3dT@cluster0.rovcj.mongodb.net/?retryWrites=true&w=majority")
-    db = client["myportfolio-db"]
-    col = db["chatbot-missed"]
+    client = pymongo.MongoClient("<MONGODB URL>")
+    db = client["<DATABASE NAME>"]
+    col = db["<COLLECTION NAME>"]
 
     dict = { "message": message}
 
